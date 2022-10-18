0a8d7c1a8c0b49463e20a15a968bddba64bb02c9
Letícia Ribeiro <103691985+leticiaribeiro16@users.noreply.github.com>
2022-10-17 22:21:52 -0300

    commit

---
+++ calc.js
@@ -0,0 +1,12 @@
+let somaFunc = require('./funcoes/soma'); //sempre que quiser carregar um modulo, usar require
+
+let subFunc = require('./funcoes/sub');
+
+let multFunc = require('./funcoes/mult');
+
+let divFunc = require('./funcoes/div');
+
+console.log(somaFunc(1,2));
+console.log(subFunc(1,2));
+console.log(multFunc(1,2));
+console.log(divFunc(1,2));
